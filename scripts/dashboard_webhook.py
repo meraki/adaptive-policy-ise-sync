94663d86a9f8c45642b25440d4f3d5035cc2d15e
joshand@cisco.com <joshand@cisco.com>
2020-07-17 12:43:05 -0500

    update functions to reflect new org data structures; first phase of multi-org support

--- scripts/dashboard_webhook.py
+++ scripts/dashboard_webhook.py
@@ -34,35 +34,37 @@ def run():
             db.webhook_url = public_url.replace("http://", "https://") + "/webhook/"
             db.save()
 
-        nets = dashboard.networks.getOrganizationNetworks(db.orgid)
-        for n in nets:
-            whid = None
-            whurl = dashboard.http_servers.getNetworkHttpServers(networkId=n["id"])
-            if len(whurl) <= 0:
-                append_log(log, "creating new webhook for network", n["id"])
-                wh = dashboard.http_servers.createNetworkHttpServer(networkId=n["id"], name="adaptive-policy-sync",
+        orgs = db.organization.all()
+        for org in orgs:
+            nets = dashboard.networks.getOrganizationNetworks(org.orgid)
+            for n in nets:
+                whid = None
+                whurl = dashboard.networks.getNetworkHttpServers(networkId=n["id"])
+                if len(whurl) <= 0:
+                    append_log(log, "creating new webhook for network", n["id"])
+                    wh = dashboard.networks.createNetworkHttpServer(networkId=n["id"], name="adaptive-policy-sync",
                                                                     url=db.webhook_url)
-                whid = wh["id"]
-            else:
-                for w in whurl:
-                    if w["name"] == "adaptive-policy-sync":
-                        append_log(log, "updating for network", n["id"])
-                        dashboard.http_servers.updateNetworkHttpServer(networkId=n["id"], id=w["id"],
+                    whid = wh["id"]
+                else:
+                    for w in whurl:
+                        if w["name"] == "adaptive-policy-sync":
+                            append_log(log, "updating for network", n["id"])
+                            dashboard.networks.updateNetworkHttpServer(networkId=n["id"], id=w["id"],
                                                                        url=db.webhook_url)
-                        whid = w["id"]
+                            whid = w["id"]
 
-            if whid:
-                al = dashboard.alert_settings.getNetworkAlertSettings(networkId=n["id"])
-                for a in al["alerts"]:
-                    if a["type"] == "settingsChanged":
-                        a["alertDestinations"]["httpServerIds"].append(whid)
-                        a["enabled"] = True
+                if whid:
+                    al = dashboard.networks.getNetworkAlertSettings(networkId=n["id"])
+                    for a in al["alerts"]:
+                        if a["type"] == "settingsChanged":
+                            a["alertDestinations"]["httpServerIds"].append(whid)
+                            a["enabled"] = True
 
-                append_log(log, "updating alert settings", al)
-                r = dashboard.alert_settings.updateNetworkAlertSettings(networkId=n["id"],
-                                                                        defaultDestinations=al["defaultDestinations"],
-                                                                        alerts=al["alerts"])
-                append_log(log, "update response", r)
+                    append_log(log, "updating alert settings", al)
+                    r = dashboard.networks.updateNetworkAlertSettings(networkId=n["id"],
+                                                                      defaultDestinations=al["defaultDestinations"],
+                                                                      alerts=al["alerts"])
+                    append_log(log, "update response", r)
     else:
         append_log(log, "Dashboard webhooks are not configured")
         db_log("dashboard_webhook", log)
