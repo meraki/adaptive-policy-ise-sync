bec451747e5501ac5a5bfb923d0762b3b8816c50
joshand@cisco.com <joshand@cisco.com>
2020-10-16 17:57:35 -0500

    add data to admin view

--- sync/admin.py
+++ sync/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from sync.models import UploadZip, Upload, Dashboard, ISEServer, SyncSession, Tag, ACL, Policy, Task, Organization
+from sync.models import UploadZip, Upload, Dashboard, ISEServer, SyncSession, Tag, ACL, Policy, Task, Organization,\
+    TagData, ACLData, PolicyData
 
 
 class OrganizationAdmin(admin.ModelAdmin):
@@ -11,19 +12,16 @@ class ISEServerAdmin(admin.ModelAdmin):
 
 
 class TagAdmin(admin.ModelAdmin):
-    readonly_fields = ('cleaned_name', 'last_update', 'last_update_data', 'match_report', 'push_delete', 'update_dest',
-                       'sourced_from', 'last_update_state', 'meraki_ver', 'ise_ver', 'needs_update', 'update_failed')
+    readonly_fields = ('cleaned_name', 'objects_desc', 'objects_match', 'objects_in_sync', 'object_update_target',
+                       'push_delete')
 
 
 class ACLAdmin(admin.ModelAdmin):
-    readonly_fields = ('last_update', 'last_update_data', 'match_report', 'push_delete', 'update_dest',
-                       'visible', 'sourced_from', 'last_update_state', 'meraki_ver', 'ise_ver', 'needs_update',
-                       'update_failed')
+    readonly_fields = ('objects_desc', 'objects_match', 'objects_in_sync', 'object_update_target', 'push_delete')
 
 
 class PolicyAdmin(admin.ModelAdmin):
-    readonly_fields = ('last_update', 'last_update_data', 'match_report', 'push_delete', 'update_dest',
-                       'sourced_from', 'last_update_state', 'meraki_ver', 'ise_ver', 'needs_update', 'update_failed')
+    readonly_fields = ('objects_desc', 'objects_match', 'objects_in_sync', 'object_update_target', 'push_delete')
 
 
 class SyncSessionAdmin(admin.ModelAdmin):
@@ -38,6 +36,21 @@ class TaskAdmin(admin.ModelAdmin):
     readonly_fields = ('task_data', 'last_update')
 
 
+class TagDataAdmin(admin.ModelAdmin):
+    readonly_fields = ('iseserver', 'organization', 'source_id', 'source_data', 'source_ver', 'last_sync',
+                       'update_dest', 'last_update', 'last_update_data', 'last_update_state')
+
+
+class ACLDataAdmin(admin.ModelAdmin):
+    readonly_fields = ('iseserver', 'organization', 'source_id', 'source_data', 'source_ver', 'last_sync',
+                       'update_dest', 'last_update', 'last_update_data', 'last_update_state')
+
+
+class PolicyDataAdmin(admin.ModelAdmin):
+    readonly_fields = ('iseserver', 'organization', 'source_id', 'source_data', 'source_ver', 'last_sync',
+                       'update_dest', 'last_update', 'last_update_data', 'last_update_state')
+
+
 admin.site.register(UploadZip)
 admin.site.register(Upload, UploadAdmin)
 admin.site.register(Dashboard)
@@ -46,6 +59,9 @@ admin.site.register(ISEServer, ISEServerAdmin)
 # admin.site.register(ISEMatrix)
 admin.site.register(SyncSession, SyncSessionAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(TagData, TagDataAdmin)
 admin.site.register(ACL, ACLAdmin)
+admin.site.register(ACLData, ACLDataAdmin)
 admin.site.register(Policy, PolicyAdmin)
+admin.site.register(PolicyData, PolicyDataAdmin)
 admin.site.register(Task, TaskAdmin)
