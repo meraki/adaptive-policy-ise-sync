b769778998eee262f7fe5d770de89b9ef77baa2c
joshand@cisco.com <joshand@cisco.com>
2020-10-20 20:30:57 -0500

    update tasks with new pxgrid script

--- scripts/tasks.py
+++ scripts/tasks.py
@@ -2,7 +2,7 @@ import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 import scripts.dashboard_monitor
 import scripts.ise_monitor
-import scripts.pxgrid_websocket
+import scripts.px_subscribe
 import scripts.dashboard_webhook  # noqa: F401
 import scripts.db_backup  # noqa: F401
 
@@ -14,6 +14,8 @@ def run():     # pragma: no cover
     cron.add_job(scripts.dashboard_monitor.sync_dashboard, 'interval', id="dashboard_monitor", seconds=10)
     cron.add_job(scripts.ise_monitor.sync_ise, 'interval', id="ise_monitor", seconds=10)
     cron.add_job(scripts.db_backup.backup, 'interval', id="db_backup", hours=24)
+    cron.add_job(scripts.px_subscribe.job, 'interval', id="pxgrid_monitor", kwargs={"scheduler": cron}, seconds=60)
+    # cron.add_job(scripts.px_subscribe.job, id="pxgrid_monitor", kwargs={"scheduler": cron})
 
     cron.start()
     atexit.register(lambda: cron.shutdown(wait=False))
